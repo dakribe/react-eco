f799f562edb8f363552664c97ba73e0bf5f23917
daltkb <daltkben@gmail.com>
2023-09-18 10:49:27 -0500

    Configure db connection

---
+++ backend/cmd/internal/datbase/postgres.go
@@ -0,0 +1,19 @@
+package datbase
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPostgres() *pgxpool.Pool {
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	return pool
+}
--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"server/cmd/internal/datbase"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	pool := datbase.NewPostgres()
+	fmt.Println(pool)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
