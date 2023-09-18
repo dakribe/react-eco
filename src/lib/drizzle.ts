4a25122e71b48aa414da3ffc5fd3431024406b31
daltkb <daltkben@gmail.com>
2023-09-18 08:36:32 -0500

    Remove next.js

--- src/lib/drizzle.ts
+++
@@ -1,9 +0,0 @@
-import pg from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-export const db = drizzle(pool);
