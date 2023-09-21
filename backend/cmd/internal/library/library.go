95678f0f3d1b0212ce8945891ab1b03d8ffc3845
daltkb <daltkben@gmail.com>
2023-09-21 14:17:18 -0500

    Fix library json schema

--- backend/cmd/internal/library/library.go
+++ backend/cmd/internal/library/library.go
@@ -1,9 +1,9 @@
 package library
 
 type Library struct {
-	ID          int
-	Name        string
-	Description string
-	RepoUrl     string
+	ID          int `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	RepoUrl     string 	`json:"repoUrl"`
 	CategoryID  int `json:"-"`
 }
