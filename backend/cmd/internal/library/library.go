d17a74e033208c9c502a4416991b4e980ebd1296
daltkb <daltkben@gmail.com>
2023-10-30 06:34:53 -0500

    reset

--- backend/cmd/internal/library/library.go
+++
@@ -1,9 +0,0 @@
-package library
-
-type Library struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	RepoUrl     string `json:"repoUrl"`
-	CategoryID  int    `json:"-"`
-}
