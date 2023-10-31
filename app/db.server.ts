cd387f937f56d38de8fe0fcad75e83abdbe43910
daltkb <daltkben@gmail.com>
2023-10-31 17:13:51 -0500

    fix prisma instance

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,5 +1,20 @@
+// app/utils/prisma.server.ts
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+let prisma: PrismaClient;
+declare global {
+  var __db: PrismaClient | undefined;
+}
 
-export default db;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+  prisma.$connect();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+    global.__db.$connect();
+  }
+  prisma = global.__db;
+}
+
+export { prisma };
