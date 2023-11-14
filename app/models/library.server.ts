5f57e40b8ec20646a2e55451bcab7b5b9bea82f9
daltkb <daltkben@gmail.com>
2023-11-14 12:29:02 -0600

    render library readme

--- app/models/library.server.ts
+++ app/models/library.server.ts
@@ -1,10 +1,21 @@
-import { Library } from "@prisma/client";
+import type { Library } from "@prisma/client";
 import { prisma } from "~/db.server";
 
 export async function getLibrary(id: Library["id"]) {
   return prisma.library.findUnique({
     where: {
       id,
+    },
+  });
+}
+
+export async function getLibraryReadme(id: Library["id"]) {
+  return prisma.library.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      readMeUrl: true,
     },
   });
 }
--- app/routes/_main.libraries.$id._index.tsx
+++ app/routes/_main.libraries.$id._index.tsx
@@ -1,15 +1,25 @@
-import { json } from "@remix-run/node";
+import { type LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { getLibraryReadme } from "~/models/library.server";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 
-export const loader = async () => {
-  return json("Repo data");
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.id, "Missing library id");
+  const readmeUrl = await getLibraryReadme(params.id);
+  const url = readmeUrl?.readMeUrl;
+
+  const res = await fetch(url);
+  const markdown = await res.text();
+  return json(markdown);
 };
 
 export default function LibrariesIndex() {
   const data = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>{data}</h1>
+      <Markdown rehypePlugins={[rehypeHighlight]}>{data}</Markdown>
     </div>
   );
 }
