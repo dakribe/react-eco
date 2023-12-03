a879a5b0e61e2dc940559dad87a23c4137e26ab9
daltkb <daltkben@gmail.com>
2023-12-03 04:52:59 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import netlify from "solid-start-netlify";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid({ adapter: netlify({ edge: false }) })],
+  plugins: [solid({ adapter: vercel() })],
   ssr: { external: ["@prisma/client"] },
 });
